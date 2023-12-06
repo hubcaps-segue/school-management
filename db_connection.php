de31a50c631255017349f73e30db8101248845ab
Kate S <152852290+hubcaps-segue@users.noreply.github.com>
2023-12-06 10:25:59 +0600

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,14 @@
+<?php
+// db_connection.php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "school_management_system";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
